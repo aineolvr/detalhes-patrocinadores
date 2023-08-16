45646cc1e469b47cb79f8cbab18d56a415f9344b
Aine Oliveira <87451233+aineolvr@users.noreply.github.com>
2023-08-16 09:47:03 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,53 @@
+
+
+
+
+const headerContent = document.querySelector('.header-content');
+const optionsCard = document.querySelector('.options-card');
+const svgIcon = document.querySelector('.header-content svg');
+
+// adicionando evento de click ao svg 
+svgIcon.addEventListener('click', () => {
+    optionsCard.classList.toggle('show');
+});
+
+//fechar o options-card quando clicar fora deles 
+document.addEventListener('click', (event) => {
+    if(!optionsCard.contains(event.target) && !svgIcon.contains(event.target)) {
+        optionsCard.classList.remove('show');
+    }
+})
+
+
+const mobileMenuButton = document.getElementById('mobile-menu-button');
+const menuContent = document.getElementById('additional-buttons');
+const BtnCancelar = document.getElementById('btn-cancelar');
+
+mobileMenuButton.addEventListener('click', () => {
+    if (menuContent.style.display === 'block') {
+        menuContent.style.display = 'none';
+    } else {
+        menuContent.style.display = 'block';
+    }
+});
+
+ 
+ const openModalButton = document.getElementById('openModalButton');
+ const modal = document.querySelector('.mdc-dialog');
+
+ 
+ openModalButton.addEventListener('click', () => {
+   modal.classList.add('mdc-dialog--open');
+ });
+
+ 
+ const closeButton = document.querySelector('.mdc-dialog__close');
+
+
+ closeButton.addEventListener('click', () => {
+   modal.classList.remove('mdc-dialog--open');
+ });
+
+ BtnCancelar.addEventListener('click', () => {
+    modal.classList.remove('mdc-dialog--open')
+ })
